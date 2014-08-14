83a3e0a7df864b9ea652cb3727b4cfc0bb2f5f78
Brian Oldak <broldak@Brians-MacBook-Pro.local>
2014-08-14 00:14:37 -0400

    add sort

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,5 @@
 var Calendar = (function(){
+  //default dom element with sample data
   var domElement = '<div class="calendar-event">' +
                       '<div class="event-inner">' +
                         '<div class="event-name">' +
@@ -19,12 +20,20 @@ var Calendar = (function(){
 
     },
 
+    //sort events, iterate through events and create a DOM element for each
     addToDom: function() {
+      //sort the events by start time
+      this.events = this.events.sort(function(a, b){
+        return a.start - b.start;
+      });
+
       $.each(this.events, $.proxy(function(index, event){
         this.createDomElement(event);
       }, this));
     },
 
+    //TODO: add checking for overlaps, etc.
+    //adds element to the DOM.
     createDomElement: function(event){
       var $elem = $(domElement),
           top = event.start + 'px',
@@ -44,7 +53,7 @@ function layOutDay(events) {
 }
 
 function testLayOutDay() {
-  var events = [{start: 30, end: 90}, {start: 60, end: 120}, {start: 300, end: 360}];
+  var events = [{start: 60, end: 120}, {start: 300, end: 360}, {start: 30, end: 90}];
 
   layOutDay(events);
 }

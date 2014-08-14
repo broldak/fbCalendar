047486a1dd42c395a53a3d66de8ea064bf46a339
Brian Oldak <broldak@Brians-MacBook-Pro.local>
2014-08-14 00:07:22 -0400

    display all events in list

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,39 @@
 var Calendar = (function(){
+  var domElement = '<div class="calendar-event">' +
+                      '<div class="event-inner">' +
+                        '<div class="event-name">' +
+                          'Sample Item' +
+                        '</div>' +
+                        '<div class="event-location">' +
+                          'Sample Location' +
+                        '</div>' +
+                      '</div>' +
+                    '</div>';
+
   return {
     init: function(events) {
-      this._events = events;
-      console.log(events);
+      this.events = events;
+      this.$calendarContainer = $('#calendar-container');
+
+      this.addToDom();
+
+    },
+
+    addToDom: function() {
+      $.each(this.events, $.proxy(function(index, event){
+        this.createDomElement(event);
+      }, this));
+    },
+
+    createDomElement: function(event){
+      var $elem = $(domElement),
+          top = event.start + 'px',
+          height = event.end - event.start + 'px';
+
+      $elem.css('top', top);
+      $elem.css('height', height);
+
+      this.$calendarContainer.append($elem);
     }
   }
 });
@@ -11,3 +42,11 @@ function layOutDay(events) {
   var calendar = new Calendar();
   calendar.init(events);
 }
+
+function testLayOutDay() {
+  var events = [{start: 30, end: 90}, {start: 60, end: 120}, {start: 300, end: 360}];
+
+  layOutDay(events);
+}
+
+testLayOutDay();
